451d09b6eece37d04356c81828eff49e148dbf77
ocy780 <113373617+ocy780@users.noreply.github.com>
2022-11-17 11:23:09 -0700

    update

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 // JavaScript Code
+
+
+// Event Listeners
+document.getElementById("ball").addEventListener("click", btnClicked);
+
+function btnClicked() {
+    document.getElementById("demo").innerHTML = "YES!";
+    document.getElementById("demo").innerHTML = "NO!";
+    document.getElementById("demo").innerHTML = "It is decidedly so.";
+    document.getElementById("demo").innerHTML = "Without a doubt";
+    document.getElementById("demo").innerHTML = "You may rely on it.";
+    document.getElementById("demo").innerHTML = "Signs point to yes";
+    document.getElementById("demo").innerHTML = "Most Likely";
+    document.getElementById("demo").innerHTML = "Don't count on it";
+    document.getElementById("demo").innerHTML = "Outlook not so good";
+    document.getElementById("demo").innerHTML = "Very Doubtful";
+    document.getElementById("demo").innerHTML = "My reply is no";
+    document.getElementById("demo").innerHTML = "My sources say no";
+}
